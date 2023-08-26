933c739ed3bc0be5a5e805b4c0ad20029aff76aa
-Haru- <kendarylausteroperez@gmail.com>
2023-08-27 00:35:26 +0800

    sidenav responsive update

---
+++ resources/js/Components/ImageContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <img :src="imageVal" alt="img" class="max-w-full h-auto mx-auto" />
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+    imageVal: {
+        type: String,
+        default: "https://via.placeholder.com/150",
+    },
+});
+</script>
