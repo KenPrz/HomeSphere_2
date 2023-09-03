b95a30bee7ca1b186d60ad85a46509cb0d139545
-Haru- <kendarylausteroperez@gmail.com>
2023-09-03 20:47:05 +0800

    added is_online and online listing to database

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Facades\DB;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -34,6 +35,9 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (Auth::check()) {
+            User::where('id', Auth::id())->update(['is_online' => true]);
+        }
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
@@ -42,6 +46,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        User::where('id', Auth::id())->update(['is_online' => false]);
+        
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
