722bbf009abc48dc13d9f1b40672295a590afc17
-Haru- <52265509+KenPrz@users.noreply.github.com>
2023-10-12 17:11:30 +0800

    added vue and routes

--- resources/js/Pages/Modes/Partials/EditMode.vue
+++ resources/js/Pages/Modes/Partials/EditMode.vue
@@ -1,6 +1,5 @@
 <script setup>
 import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
