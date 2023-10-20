3bb11e95db5f6add7296a819aa90dadf5b02f2ae
Jazel29 <118753856+Jazel29@users.noreply.github.com>
2023-10-20 10:32:11 +0800

    added search functionality

--- app/Http/Controllers/AppliancesController.php
+++ app/Http/Controllers/AppliancesController.php
@@ -6,30 +6,33 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AppUtilities;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+
 class AppliancesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
+{
+    $appUtilities = new AppUtilities();
+    $user = auth()->user();
+    $homeData = $appUtilities->findHomeData($user);
+    $appliances = $this->getAppliances($homeData);
+
+    return Inertia::render('Appliances/Main', [
+        'appliances' => $appliances,
+    ])->withViewData(['appliances' => $appliances]);
+}
+
+
+    public function getAppliances($homeData)
     {
-        
-        $appUtilities = new AppUtilities();
-        $user = auth()->user();
-        $homeData = $appUtilities->findHomeData($user);
-        $appliances = $this->getAppliances($homeData);
-        // Perform any additional operations on $appliances if needed
-    
-        return Inertia::render('Appliances/Main', [
-            'appliances' => $appliances,
-        ]);
-    }
-    public function getAppliances($homeData){
         return DB::table('devices')
-                ->select(
-                    'rooms.room_name',
-                    'devices.device_type',
-                    'devices.device_name',
-                    DB::raw('CASE WHEN devices.is_active = 1 THEN "Active" ELSE "Inactive" END AS is_active')
-                )->where('rooms.home_id', $homeData->id)
-                ->join('rooms', 'devices.room_id', '=', 'rooms.id')
-                ->get();
+            ->select(
+                'rooms.room_name as Room',
+                'devices.device_type as Type',
+                'devices.device_name',
+                DB::raw('CASE WHEN devices.is_active = 1 THEN "Active" ELSE "Inactive" END AS is_active')
+            )
+            ->where('rooms.home_id', $homeData->id)
+            ->join('rooms', 'devices.room_id', '=', 'rooms.id')
+            ->get();
     }
 }
--- resources/js/Pages/Appliances/Main.vue
+++ resources/js/Pages/Appliances/Main.vue
@@ -1,53 +1,75 @@
 <template>
     <Head title="Appliances" />
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Appliances
-            </h2>
-        </template>
-        <main>
-            <div class="container">
-                    <h1 class= "mx-10 py-3 text-3xl font-bold">
-                        List of Appliances
-                    </h1>
-                    <div class="flex-grow mx-10 mt-3 p-3 rounded-lg bg-white">
-                        <div class="flex items-center border border-slate-500 py-1 sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 h-12 rounded-full text-1xl text-left">
-                        <div class="w-full pe-5 flex">
-                        <div class="ml-5 self-center">
-                            <img src="img-assets/vectors/search.svg" alt="search">
-                        </div>
-                        <input id="search" placeholder="Search" class="ms-5 w-full border-0 focus:ring-0">
-                        </div>
-                    </div>
-                    </div>
-                    <div class="mx-10 mt-5">
-                        <Table
-                            :tableHeaders="tableHeaders"
-                            :tableData="tableData"
-                            :maxHeight="maxHeight"
-                            :itemsPerPage="6"
-                            :Pagenated="true"
-                        />
-                    </div>
+      <template #header>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          Appliances
+        </h2>
+      </template>
+      <main>
+        <div class="container">
+            <h1 class="mx-10 py-3 text-3xl font-bold">
+            List of Appliances
+          </h1>
+          <div class="flex-grow mx-10 mt-3 p-3 rounded-lg bg-white">
+            <div class="flex items-center border border-slate-500 py-1 sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 h-12 rounded-full text-1xl text-left">
+              <div class="w-full pe-5 flex">
+                <div class="ml-5 self-center">
+                  <img src="img-assets/vectors/search.svg" alt="search" />
+                </div>
+                <input
+                  v-model="searchInput"
+                  id="search"
+                  name="searchInput"
+                  placeholder="Search"
+                  class="ms-5 w-full border-0 focus:ring-0"
+                />
+              </div>
             </div>
-        </main>
+          </div>
+          <div class="mx-10 mt-5">
+            <Table
+              :tableHeaders="tableHeaders"
+              :tableData="filteredTableData"
+              :maxHeight="maxHeight"
+              :itemsPerPage="6"
+              :Pagenated="true"
+            />
+            <div v-if="filteredTableData.length === 0" class="text-center mt-4 text-red-500">
+            No Data Found
+          </div>
+          </div>
+        </div>
+      </main>
     </AuthenticatedLayout>
 </template>
-<script setup>
-import Table from "@/Components/Table.vue";
 
+<script setup>
+import { ref, computed } from "vue";
+import Table from "@/Components/Table.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
-defineProps({
-    mustVerifyEmail: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
+
+const { appliances } = defineProps(["appliances"]);
+
+const searchInput = ref(""); // Define the searchInput variable
+const tableData = ref(appliances); 
+
+const filteredTableData = computed(() => {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+  if (!searchTerm) {
+    return tableData.value;
+  }
+
+  return tableData.value.filter((item) => {
+    const itemName = item.Room.toLowerCase() + item.Type.toLowerCase();
+
+    return itemName.includes(searchTerm);
+  });
 });
 </script>
+
 <script>
 export default {
     components: {
