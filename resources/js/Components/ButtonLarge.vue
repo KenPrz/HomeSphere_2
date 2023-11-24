4d0684173a779517b876f163dd1e4c551286ee4d
-Haru- <52265509+KenPrz@users.noreply.github.com>
2023-11-24 13:05:00 +0800

    Update font styles in ButtonLarge.vue

--- resources/js/Components/ButtonLarge.vue
+++ resources/js/Components/ButtonLarge.vue
@@ -1,7 +1,7 @@
 <template>
     <li class="lg:px-4 lg:py-6 flex items-center justify-between mb-2 rounded-lg bg-gray-600 p-4 transition duration-500 hover:bg-gray-700 hover:cursor-pointer lg:hover:translate-x-[-12px]">
     <div>
-        <span class="text-white text-md font-semibold">{{ label }}</span><br />
+        <span class="text-white text-normal md:text-md font-normal md:font-semibold">{{ label }}</span><br />
         <p class="text-md text-white">{{ text }}</p>
     </div>
     <div id="arrow" class="transform transition duration-500">
