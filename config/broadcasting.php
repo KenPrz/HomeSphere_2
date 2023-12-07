7ee3fd0d86588d1ce9184e20a7dcf8360b1bb613
-Haru- <52265509+KenPrz@users.noreply.github.com>
2023-12-07 16:47:51 +0800

    removed websockets beyondcode

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,13 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST'),
-                'port' => env('PUSHER_PORT'),
-                'scheme' => env('PUSHER_SCHEME'),
-                'encrypted' => false,
-                'useTLS' => false,
-            ],
+                'cluster' => 'ap1',
+                'useTLS' => true
+              ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
